86da5d6c7f9df83af1d91e473058513aa4952010
nisan250 <nisan250@gmail.com>
2018-12-08 11:44:14 +0000

    small changes + 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,28 +6,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './products/product-data';
 
-// third party
-import { NgxLoadingModule } from 'ngx-loading';
-// -----------------
-
-import { AppRoutingModule } from './app-routing.module';
-import { ProductModule } from './products/product.module';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+// Third Party
+import { NgxLoadingModule } from 'ngx-loading';
+
+// Feature
+import { AppRoutingModule } from './app-routing.module';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    NgxLoadingModule.forRoot({}),
     HttpClientModule,
-    InMemoryWebApiModule.forRoot(ProductData),
+    InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 } ),
     ProductModule,
     AppRoutingModule,
-    NgxLoadingModule.forRoot({}),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+    <h1>sorry, that page doesn't exist!</h1>
+    `
+})
+export class PageNotFoundComponent { }
